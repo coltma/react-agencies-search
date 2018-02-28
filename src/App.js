e4549aa6b7f093fe73185c7ad3c0fa67e7f83a94
coltma <shawma.dev@gmail.com>
2018-02-28 12:54:33 -0600

    added layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import SearchAgency from './containers/SearchAgency';
 import './App.css';
-
-
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SearchAgency />
+      <div>
+      <Layout>
+        <Header style={{background: '#e1f2ee'}}><div className="logo">
+          <img
+            src="https://webassets.amne.co/3.0/AmneLogo.png"
+            alt="Amne Logo" style={{height: 35}}/></div></Header>
+        <Content><SearchAgency /></Content>
+        <Footer></Footer>
+      </Layout>
       </div>
     );
   }
