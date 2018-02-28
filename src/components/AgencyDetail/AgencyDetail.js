82115fac6f4307f23d238078f8bdef3353c18f05
coltma <shawma.dev@gmail.com>
2018-02-27 22:26:18 -0600

    add new AgencyDetail

---
+++ src/components/AgencyDetail/AgencyDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from 'antd';
+
+//
+const AgencyDetail = (props) => (
+  <Card style={{ height: 100 }}>
+    <p>Card content</p>
+    <p>Card content</p>
+
+  </Card>
+);
+
+export default AgencyDetail;
